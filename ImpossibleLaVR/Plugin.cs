6625e3c895d5db99cd7aac9a80676102f149098b
Alec Carlyle <cyheddie@gmail.com>
2023-02-05 18:35:06 -0500

    Initial render of impossible lair in VR

---
+++ ImpossibleLaVR/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using Valve.VR;
+
+namespace ImpossibleLaVR
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "com.mlkamano.VRMods.ImpossibleLaVR";
+        public const string PLUGIN_NAME = "ImpossibleLaVR";
+        public const string PLUGIN_VERSION = "0.0.1";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            InitSteamVR();
+        }
+
+        private static void InitSteamVR()
+        {
+            SteamVR.Initialize(true);
+            SteamVR_Settings.instance.pauseGameWhenDashboardVisible = true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ImpossibleLaVR
 VR conversion mod for Yooka Laylee and the Impossible Lair
+
+# Quick Dev Notes
+2017.4.23 (2017.4.23f1) unity version
+
+# Thanks
+* Raicuparta for his VR Patcher code from TwoForksVR <3
+* Herr "Punished" Fristi for his VR modding tutorials (:
